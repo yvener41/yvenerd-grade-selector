41de8bdcc671abd6ca576b5183ce35bf9706dab3
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-04-08 23:30:07 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  var score = 100;
+  var grade;
+
+  if (score >= 90) {
+    grade = "A";
+  } else if (score >= 80) {
+    grade = "B";
+  } else if (score >= 70) {
+    grade = "C";
+  } else if (score >= 60) {
+    grade = "D";
+  } else {
+    grade = "F";
+  }
+  console.log("Grade: " + grade);
 };
